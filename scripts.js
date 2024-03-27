31ded5bc29d2816fdd405e77a6d01ddb78797faa
luiza <luiza.leal@hsl.org.br>
2024-03-27 15:42:35 -0300

    Update visual

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 // scripts.js
 // Currently, there are no interactive elements specified. This file is prepared for any future JavaScript functionality.
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
+
+
